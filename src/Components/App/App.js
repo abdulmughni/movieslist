96f314f45dcb9403832544f0a06d32664bad602c
abdulmughni <abdul.mughni94@gmail.com>
2020-06-22 17:52:09 +0500

    Complete task of movies list

---
+++ src/Components/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import ContentArea from '../';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="main-wrapper">
+      <ContentArea />
+    </div>
+  );
+}
+
+export default App;
